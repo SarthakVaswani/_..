938fed3631feb9780e4b4ef3f91851573b0fee95
svas0000 <svas0000@gmail.com>
2020-06-03 17:17:30 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,9 +1,9 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "android-chrome-192x192.png": "d747bb448e2601cca8963639acd0ddcc",
-"android-chrome-512x512.png": "50428f84e0bc789022d8040137909bcf",
-"apple-touch-icon.png": "21eafada1eb16e90802b56106803b043",
+  "android-chrome-192x192.png": "f416bb7d45caa131c21f74b0d3264c10",
+"android-chrome-512x512.png": "0166356c4dc414fd7331d06b5563c70c",
+"apple-touch-icon.png": "d7419387d353134c9a241c01fd030dc8",
 "assets/AssetManifest.json": "8219297c9583868dba3dfd8082c6f87d",
 "assets/assets/fonts/Exo2-VariableFont_wght.ttf": "afd53ab0d9cb6142bfb815b8c9226401",
 "assets/assets/images/github-logo.png": "3e22b5ef18f9a7c3e1a68c842dd13159",
@@ -14,16 +14,17 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/LICENSE": "7f1df1ee47854388e684afc0845ea820",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"favicon-16x16.png": "088eee77ed00e43433fcb05bb1b5a775",
-"favicon-32x32.png": "1043bb3eeb6dacef5fb2a59f64b56878",
-"favicon.ico": "890a8bb695a4fcc03528c323ed924291",
+"favicon-16x16.png": "da6658433df00c401c4025dacf2da156",
+"favicon-32x32.png": "0e5e90f05e0d861fc1d3db8eb5aa8e3b",
+"favicon.ico": "9912afd24981e603fb3e41f89a109356",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "fd38216d7c49e9cd475b330e3c695b22",
 "/": "fd38216d7c49e9cd475b330e3c695b22",
 "main.dart.js": "a459962b3a9c5a08d0872e59d2ee8ca3",
-"manifest.json": "35d6110b8786714dd40187f90d568dc3"
+"manifest.json": "35d6110b8786714dd40187f90d568dc3",
+"site.webmanifest": "053100cb84a50d2ae7f5492f7dd7f25e"
 };
 
 self.addEventListener('activate', function (event) {
